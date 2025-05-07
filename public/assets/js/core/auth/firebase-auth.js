f55015c58c5e4872c5cfd1e193e8a2305d05cdc0
Tiago Diniz <tiagodnzz@gmail.com>
2025-05-07 20:51:05 -0300

    feat: ajustando rotas

--- public/assets/js/core/auth/firebase-auth.js
+++ public/assets/js/core/auth/firebase-auth.js
@@ -24,7 +24,7 @@ if (loginForm) { // Executa apenas se o formulário existir na página atual (lo
             .then((userCredential) => {
                 console.log('Login OK:', userCredential.user);
                 // Redireciona para o dashboard na raiz
-                window.location.href = '../admin/dashboard.html';
+                window.location.href = '/dashboard';
             })
             .catch((error) => {
                 console.error('Erro no login:', error);
@@ -40,8 +40,8 @@ if (loginForm) { // Executa apenas se o formulário existir na página atual (lo
 auth.onAuthStateChanged((user) => {
     const currentPage = window.location.pathname.split('/').pop();
     // Ajuste os nomes dos arquivos conforme os seus HTMLs na raiz
-    const isLoginPage = currentPage === 'login.html';
-    const isDashboardPage = currentPage === 'dashboard.html';
+    const isLoginPage = currentPage === 'login';
+    const isDashboardPage = currentPage === 'dashboard';
     const isAdminPage = isLoginPage || isDashboardPage; // Adicione outras páginas admin se houver
 
     console.log(`Auth state: user=${user ? user.uid : null}, page=${currentPage}, isAdmin=${isAdminPage}`);
@@ -51,14 +51,14 @@ auth.onAuthStateChanged((user) => {
         // Se estiver na página de login -> vai pro dashboard
         if (isLoginPage) {
             console.log("Logado, redirecionando do login para o dashboard...");
-            window.location.href = '../admin/dashboard.html';
+            window.location.href = '/dashboard';
         }
     } else {
         // Usuário NÃO LOGADO
         // Se estiver em alguma página admin protegida (dashboard) -> vai pro login
         if (isDashboardPage) { // Adicione outras páginas protegidas aqui com ||
             console.log("Não logado, redirecionando do dashboard para o login...");
-            window.location.href = '../auth/login.html';
+            window.location.href = '/login';
         }
     }
 });
@@ -70,7 +70,7 @@ function adminLogout() { // Renomeando para evitar conflitos potenciais
             console.log('Logout bem-sucedido.');
             // Após o logout, o estado do usuário será null, e o onAuthStateChanged será chamado
             // O que vai redirecionar para a página de login
-            window.location.href = '../auth/login.html'; // Redireciona explicitamente para login
+            window.location.href = '/login'; // Redireciona explicitamente para login
         })
         .catch((error) => {
             console.error('Erro no logout:', error);
